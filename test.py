6030bddec4c6cc48f4bb772716db8a2898a604a9
root <root@OpenWrt.lan>
2020-11-11 11:27:14 +0000

    Update test.py

--- test.py
+++ test.py
@@ -2,10 +2,12 @@ import openwrtRPiGpio as GPIO
 from time import sleep
 
 pin=GPIO.pin(2,GPIO.OUTPUT)
-
-while(1):
+i=0
+while(i<10):
     pin.write(1)
     sleep(0.5)
     pin.write(0)
+    i+=1
 
 
+pin.cleanup()
