8edb3cf71b140864b1b0fee0486de86f72b8567d
Harsh Singhvi <root@harshsinghvi.com>
2020-11-17 12:28:30 +0530

    Optimising

--- examples/WebRelayAPI/app.py
+++ examples/WebRelayAPI/app.py
@@ -1,27 +1,26 @@
+import sys
 import flask
 from flask_cors import CORS
-from flask import request, jsonify,render_template
-import os
+from flask import request, render_template
 import UniversalGPIO.GPIO as GPIO
-import sys
 
-PINS={
-    "R1": 2,
-    "R2": 3,
-    "R3": 14,
+PINS = {
+    "R1": 2, 
+    "R2": 3, 
+    "R3": 14, 
     "R4": 15
 }
-relay_state={
-    "R1":0,
-    "R2":0,
-    "R3":0,
-    "R4":0,
+relay_state = {
+    "R1":0, 
+    "R2":0, 
+    "R3":0, 
+    "R4":0, 
 }
-gpio={}
+gpio = {}
 
 def pin_init():
     for pin in PINS:
-        gpio[pin]=GPIO.setup(PINS[pin],GPIO.OUTPUT,reverse_state=True,initial_state=0)
+        gpio[pin]=GPIO.setup(PINS[pin], GPIO.OUTPUT, reverse_state=True, initial_state=0)
 def gpio_cleanup():
         for pin in PINS:
             gpio[pin].cleanup()
@@ -30,40 +29,40 @@ app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 CORS(app)
 
-@app.route('/hello-world',methods=['GET','POST'])
+@app.route('/hello-world', methods=['GET', 'POST'])
 def hello_world():
     return "Hello World !!"
-@app.route('/status',methods=['GET'])
+@app.route('/status', methods=['GET'])
 def status():
     return relay_state
-@app.route('/status/<relay>',methods=['GET'])
+@app.route('/status/<relay>', methods=['GET'])
 def relay_status(relay):
     if relay in PINS:
         return str(relay_state[relay])
     else:
-        return "Bad Request",400
-@app.route('/set',methods=['GET','POST'])
+        return "Bad Request", 400
+@app.route('/set', methods=['GET', 'POST'])
 def set():
     if 'relay' in request.args and 'state' in request.args:
         gpio[request.args["relay"]].write(int(request.args['state']))
         relay_state[request.args['relay']]=int(request.args['state'])
         return "OK"
     else:
-        return "Bad Request",400       
-@app.route('/set/<relay>/<state>',methods=['GET','POST'])
-def relay_set(relay,state):
+        return "Bad Request", 400       
+@app.route('/set/<relay>/<state>', methods=['GET', 'POST'])
+def relay_set(relay, state):
     if (int(state)==1 or int(state)==0) and (relay in PINS):
         gpio[relay].write(int(state))
         relay_state[relay]=int(state)
         return "OK"
-    return "Bad Request",400
-@app.route('/',methods=['GET'])
+    return "Bad Request", 400
+@app.route('/', methods=['GET'])
 def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
     try:
         pin_init()
-        app.run(host="0.0.0.0",port=5000)
+        app.run(host="0.0.0.0", port=5000)
     except KeyboardInterrupt:
         sys.exit()
