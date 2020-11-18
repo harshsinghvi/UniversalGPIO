7b7a410d6fe08ef6369a5bd21fdbe97389ece780
Harsh Singhvi <root@harshsinghvi.com>
2020-11-18 23:05:48 +0530

    ⏭️ Preparing Version Release

--- UniversalGPIO/__init__.py
+++ UniversalGPIO/__init__.py
@@ -9,7 +9,7 @@ includes Modules:
  
 
 '''
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 
 def version():
 	return __version__
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/harshsinghvi/UniversalGPIO'
 EMAIL = 'root@harshsinghvi.com'
 AUTHOR = 'Harsh Singhvi'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '1.0.3'
+VERSION = '1.0.4'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
