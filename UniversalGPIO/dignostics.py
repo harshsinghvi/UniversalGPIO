44b9fab3cda162fd1a271646f934aef253f6789f
Harsh Singhvi <root@harshsinghvi.com>
2020-12-09 22:45:05 +0530

    create dignostics module

---
+++ UniversalGPIO/dignostics.py
@@ -0,0 +1,37 @@
+from UniversalGPIO.GPIO import Pin, OUT,IN,ON,OFF
+from os import system
+from time import sleep
+
+modes = ['out','output','in','input']
+states= ['on', 'off', 1, 0, 'high', 'low']
+
+def gpiocontroll(pin, mode, state=0):
+    if mode not in modes:
+        raise InvalidMode("allowed_modes =['out','output','in','input']")
+    if state not in states:
+        raise InvalidMode("allowed_STATES =['on', 'off', 1, 0, 'high', 'low']")
+    else:
+        if state in ['on', 1, 'high']
+            state = 1
+        else: 
+            state = 0
+    temp=Pin(pin,mode,state)
+    
+def unexport(pin):
+    system("echo "+str(pin)+ " > /sys/class/gpio/unexport")
+
+def blink_one_buy_one(delay_time, start=None, end=None):
+    if start == None and end == None:
+        system("./scripts/pin_discovery_blink.sh {}".format(delay_time))
+    else:
+        system("./scripts/pin_discovery_blink.sh {} {} {}".format(delay_time, start, end))
+
+def blink_all(start, end):
+    system("./scripts/pin_discovery_blink-all.sh {} {}".format(start, end))
+    
+def gpiocontroll():
+    pass
+
+def gpiocontroll():
+    pass
+
